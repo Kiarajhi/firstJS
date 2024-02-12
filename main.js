a97a9175b9e6444804a1cc6f0da65cff7f2de1d4
Kiara Hill <Kiarahill2025@u.northwestern.edu>
2024-02-12 15:27:33 -0800

    Added JS

---
+++ main.js
@@ -0,0 +1,22 @@
+const header =  document.querySelector("#header")
+const image = document.querySelector("#image")
+const credit =  document.querySelector("#credit")
+
+document.querySelector("#chanterelle-button").addEventListener("click", function() {
+    document.querySelector("#header").innerHTML = ("Chanterelle")
+    document.querySelector("#image").src = "chanterelle.jpg"
+    document.querySelector("#credit").innerHTML="Neil Leclerc"
+})
+
+ document.querySelector("#oyster-button").addEventListener("click", function() {
+       header.innerHTML = ("Oyster mushroom")
+        image.src = "oyster.jpg"
+        credit.innerHTML="Robbie Spoule"  
+ })
+
+document.querySelector("#fly-agaric-button").addEventListener("click", function() {
+            header.innerHTML = ("Fly agaric")
+             image.src = "fly-agaric.jpg"
+             credit.innerHTML="Victoria Someone"  
+
+})
