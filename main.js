0249d1023993400cc5ef28c5fafc9c8a872231a9
Kiara Hill <Kiarahill2025@u.northwestern.edu>
2024-02-12 15:48:22 -0800

    Updated credit

--- main.js
+++ main.js
@@ -5,18 +5,18 @@ const credit =  document.querySelector("#credit")
 document.querySelector("#chanterelle-button").addEventListener("click", function() {
     document.querySelector("#header").innerHTML = ("Chanterelle")
     document.querySelector("#image").src = "chanterelle.jpg"
-    document.querySelector("#credit").innerHTML="Neil Leclerc"
+    document.querySelector("#credit").innerHTML="© Neil Leclerc"
 })
 
  document.querySelector("#oyster-button").addEventListener("click", function() {
        header.innerHTML = ("Oyster mushroom")
         image.src = "oyster.jpg"
-        credit.innerHTML="Robbie Spoule"  
+        credit.innerHTML="© Robbie Sproule"  
  })
 
 document.querySelector("#fly-agaric-button").addEventListener("click", function() {
             header.innerHTML = ("Fly agaric")
              image.src = "fly-agaric.jpg"
-             credit.innerHTML="Victoria Someone"  
+             credit.innerHTML="© Victoria Vasilieva"  
 
 })
